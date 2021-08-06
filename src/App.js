1e446d5f671af13f5a124ce4878c51233437bf20
kdayanalm15 <dayanalasso15@gmail.com>
2021-08-06 10:26:43 -0500

    refator

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        luchar por el amor y mantener tu honor intacto.
         </p>
         <a
           className="App-link"
